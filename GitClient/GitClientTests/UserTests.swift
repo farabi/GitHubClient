7126b0e99b4af9697b868a5ecebde365d05a36c0
Saad El Oulladi <>
2019-04-28 23:47:21 +0200

    Add first User model Unit test.

--- GitClient/GitClientTests/UserTests.swift
+++ GitClient/GitClientTests/UserTests.swift
@@ -9,22 +9,44 @@
 import Quick
 import Nimble
 
+@testable import GitClient
+
 class UserTests: QuickSpec {
-
-
-    func testJSONMapping() throws {
-        let bundle = Bundle(for: type(of: self))
-        
-        guard let url = bundle.url(forResource: "User", withExtension: "json") else {
-            XCTFail("Missing file: User.json")
-            return
-        }
-        
-        let json = try Data(contentsOf: url)
-        let user: User = try unbox(data: json)
-        
-        XCTAssertEqual(user.name, "John")
-        XCTAssertEqual(user.age, 29)
+    
+    enum FakeData {
+        static let login = "kzaher"
+        static let contributions = 1686
+        static let avatarUrl = "https://avatars1.githubusercontent.com/u/1641148?v=4"
     }
-
+    
+    override func spec() {
+        var testSubject: User!
+        
+        describe("User Model") {
+            
+            context("When parsed from json") {
+                
+                beforeEach {
+                    let bundle = Bundle(for: type(of: self))
+                    guard let url = bundle.url(forResource: "User", withExtension: "json") else {
+                        fail("Json file doesn't exist")
+                        return
+                    }
+                    do {
+                        let json = try Data(contentsOf: url)
+                        testSubject = try JSONDecoder().decode(User.self, from: json)
+                    } catch {
+                        fail("Wrong json format")
+                    }
+                }
+                
+                it("Should have the expected values") {
+                    expect(testSubject.login).to(equal(FakeData.login))
+                    expect(testSubject.contributionsCount).to(equal(FakeData.contributions))
+                    expect(testSubject.avatarUrlString).to(equal(FakeData.avatarUrl))
+                }
+            }
+            
+        }
+    }
 }
