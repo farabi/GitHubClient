6f9df076db4b3ec3065d298e44ed5c7f8025e9d8
Saad El Oulladi <>
2019-04-28 21:17:14 +0200

    Make a generic base class for api calls.

--- GitClient/GitClient/Common/Api/GitHubApi.swift
+++ GitClient/GitClient/Common/Api/GitHubApi.swift
@@ -33,140 +33,67 @@ protocol ApiInterface : class {
     func fetchRemoteImage(withUrl urlString:String?) -> Observable<UIImage>
 }
 
-class GitHubApi : ApiInterface {
-
+class BaseApi {
     @discardableResult
-    func searchRepository(withQuery query:String) -> Observable<[Repository]> {
-        
+    fileprivate func apiRequest<Model>(withUrl requestUrlString:String) ->  Observable<Model> where Model:Codable {
         return Observable.create { observer -> Disposable in
-            
-            let requestUrlString = Constant.search.path(withParameter: query)
             Alamofire.request(requestUrlString).responseData(completionHandler: { response in
-                
                 if let error = response.error {
                     observer.onError(error)
                     return
                 }
-                
                 guard let responseData = response.result.value else {
                     observer.onError(GitHubApiError.unknown)
                     return
                 }
-                
                 do {// Check for parsing error
-                    let baseResponse = try JSONDecoder().decode(BaseResponse.self, from: responseData)
-                    guard let repositories = baseResponse.items else {
-                        observer.onError(GitHubApiError.wrongRequest)
-                        return
-                    }
-                    observer.onNext(repositories)
+                    let model = try JSONDecoder().decode(Model.self, from: responseData)
+                    observer.onNext(model)
                     observer.onCompleted()
                 } catch {
                     observer.onError(GitHubApiError.parsingError)
                 }
             })
-            
             return Disposables.create()
+        }
+    }
+}
+
+class GitHubApi: BaseApi, ApiInterface {
+
+    @discardableResult
+    func searchRepository(withQuery query:String) -> Observable<[Repository]> {
+        let requestUrlString = Constant.search.path(withParameter: query)
+        let apiRequestObservable:Observable<BaseResponse> = apiRequest(withUrl: requestUrlString)
+        return apiRequestObservable.map { (baseResponse) -> [Repository] in
+            guard let repositories = baseResponse.items else {
+                throw GitHubApiError.wrongRequest
+            }
+            return repositories
         }
     }
     
     @discardableResult
     func fetchContributors(byRepositoryName repositoryName:String) -> Observable<[User]> {
-        
-        return Observable.create { observer -> Disposable in
-            
-            let requestUrlString = Constant.contributors.path(withParameter: repositoryName)
-            Alamofire.request(requestUrlString).responseData(completionHandler: { response in
-                
-                if let error = response.error {
-                    observer.onError(error)
-                    return
-                }
-                
-                guard let responseData = response.result.value else {
-                    observer.onError(GitHubApiError.unknown)
-                    return
-                }
-                
-                do {
-                    let collaborators = try JSONDecoder().decode([User].self, from: responseData)
-                    observer.onNext(collaborators)
-                    observer.onCompleted()
-                } catch {
-                    observer.onError(GitHubApiError.parsingError)
-                }
-            })
-            
-            return Disposables.create()
-        }
+        let requestUrlString = Constant.contributors.path(withParameter: repositoryName)
+        return apiRequest(withUrl: requestUrlString)
     }
     
     @discardableResult
     func fetchIssues(byRepositoryName repositoryName:String) -> Observable<[Issue]> {
-        
-        return Observable.create { observer -> Disposable in
-            
-            let requestUrlString = Constant.issues.path(withParameter: repositoryName)
-            Alamofire.request(requestUrlString).responseData(completionHandler: { response in
-                
-                if let error = response.error {
-                    observer.onError(error)
-                    return
-                }
-                
-                guard let responseData = response.result.value else {
-                    observer.onError(GitHubApiError.unknown)
-                    return
-                }
-                
-                do {
-                    let issues = try JSONDecoder().decode([Issue].self, from: responseData)
-                    observer.onNext(issues)
-                    observer.onCompleted()
-                } catch {
-                    observer.onError(GitHubApiError.parsingError)
-                }
-            })
-            
-            return Disposables.create()
-        }
+        let requestUrlString = Constant.issues.path(withParameter: repositoryName)
+        return apiRequest(withUrl: requestUrlString)
     }
     
     @discardableResult
     func fetchPullRequets(byRepositoryName repositoryName:String) -> Observable<[Issue]> {
-        
-        return Observable.create { observer -> Disposable in
-            
-            let requestUrlString = Constant.pullRequests.path(withParameter: repositoryName)
-            Alamofire.request(requestUrlString).responseData(completionHandler: { response in
-                
-                if let error = response.error {
-                    observer.onError(error)
-                    return
-                }
-                
-                guard let responseData = response.result.value else {
-                    observer.onError(GitHubApiError.unknown)
-                    return
-                }
-                
-                do {
-                    let issues = try JSONDecoder().decode([Issue].self, from: responseData)
-                    observer.onNext(issues)
-                    observer.onCompleted()
-                } catch {
-                    observer.onError(GitHubApiError.parsingError)
-                }
-            })
-            
-            return Disposables.create()
-        }
+        let requestUrlString = Constant.pullRequests.path(withParameter: repositoryName)
+        return apiRequest(withUrl: requestUrlString)
     }
     
     @discardableResult
     func fetchRemoteImage(withUrl urlString:String?) -> Observable<UIImage> {
         guard let urlString = urlString else { return Observable.error(GitHubApiError.unknown) }
-        
         return Observable.create { observer -> Disposable in
             Alamofire.request(urlString).responseImage { response in
                 if let image = response.result.value {
